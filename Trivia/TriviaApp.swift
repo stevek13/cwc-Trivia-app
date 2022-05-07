658184474118d40fb5945973fd6c962bab8d6a28
Steve Kite <skite1@hotmail.com>
2022-05-07 13:55:35 -0700

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Steve Kite on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
